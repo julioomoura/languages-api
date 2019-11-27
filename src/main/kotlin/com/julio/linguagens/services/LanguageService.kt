edf761a28eadabd2b6c222b7d70291d68a1784bf
Júlio Moura <julio.moura@aisdigital.com.br>
2019-11-26 22:41:51 -0300

    Implement language service

---
+++ src/main/kotlin/com/julio/linguagens/services/LanguageService.kt
@@ -0,0 +1,19 @@
+package com.julio.linguagens.services
+
+import com.julio.linguagens.models.Language
+import javassist.NotFoundException
+
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+interface LanguageService {
+
+    fun createLanguage(language: Language): Language
+    fun deleteLanguage(id: Long)
+    fun getLanguages(): MutableIterable<Language>
+
+    @Throws(NotFoundException::class)
+    fun getLanguage(id: Long): Language
+    fun updateLanguage(id: Long, language: Language): Language
+}
