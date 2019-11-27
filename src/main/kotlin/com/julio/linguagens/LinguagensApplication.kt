15524cbaf2344b0e2892ce777c2db8a84a575456
Júlio Moura <julio.moura@aisdigital.com.br>
2019-11-26 22:07:19 -0300

    Project quickstart

---
+++ src/main/kotlin/com/julio/linguagens/LinguagensApplication.kt
@@ -0,0 +1,11 @@
+package com.julio.linguagens
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinguagensApplication
+
+fun main(args: Array<String>) {
+	runApplication<LinguagensApplication>(*args)
+}
