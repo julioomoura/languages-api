a17e2e90fa0f1b33c1d299325fda85b76054a55a
Júlio Moura <julio.moura@aisdigital.com.br>
2019-11-26 22:16:44 -0300

    Implement language repository

---
+++ src/main/kotlin/com/julio/linguagens/repositories/LanguageRepository.kt
@@ -0,0 +1,9 @@
+package com.julio.linguagens.repositories
+
+import com.julio.linguagens.models.Language
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface LanguageRepository : CrudRepository <Language, Long>{
+}
